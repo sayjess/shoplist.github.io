31cd919e49848337818cbedc5b61d3824564c519
Jess Elardo <54670886+sayjess@users.noreply.github.com>
2022-08-30 05:47:35 +0800

    Fix issue with adding h2 content to the DOM
    Added some css to the file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var deleteButton = document.getElementsByClassName("deleteButton");
 var i = document.getElementsByTagName("i");
 var secondSection = document.querySelector('.second-section');
 
+
 function inputLength() {
 	return input.value.length;
 }
@@ -13,17 +14,11 @@ function inputLength() {
 
 function deleteElement() {
 	var h2 = document.querySelector("h2");
-	if (li.length > 0){
-		h2.parentNode.removeChild(h2);
-		
-	}
+	// Add a condition to see H2 exists
+    if (h2 && li.length > 0){
+        h2.parentNode.removeChild(h2);
+    }
 }
-
-// for (let index = 0; index == li.length; index++) {
-// 	if (li.length == 0){
-// 		h2[index].remove();
-// 	}
-// }
 
 
 function createListElement() {
@@ -35,69 +30,86 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = ""; //resets the input field
 	addDeleteButtonToList();
-	deleteElement();
+	deleteElement(); //deletes h2 element when li.length > 0
+	
 
 	function addDeleteButtonToList(){
-		var button = document.createElement("deleteButton");
-		var i = document.createElement("i");
-		i.classList.add("fa-solid", "fa-trash-can");
-		button.classList.add("deleteButton");
-		button.appendChild(i);
-		button.addEventListener('click', deleteListElement);
-		li.appendChild(button);
-	}
+		var button = document.createElement("button");
+        var i = document.createElement("i");
+        i.classList.add("fa-solid", "fa-trash-can");
+        button.classList.add("deleteButton");
+        button.appendChild(i);
+        // Call common function to attach ONE handler
+        activateDeleteAction(button);
+        li.appendChild(button);
+    }   
+	// 	var button = document.createElement("button");
+	// 	var i = document.createElement("i");
+	// 	i.classList.add("fa-solid", "fa-trash-can");
+	// 	button.classList.add("deleteButton");
+	// 	button.appendChild(i);
+	// 	// console.log("deleteListElement function added");
+	// 	button.addEventListener('click', deleteListElement);
+	// 	// console.log("emptyList function added");
+	// 	li.appendChild(button);
+	// 	emptyList();
+	// }
+	
 	
 }
 
-function deleteListElement(){
-	var deletes = document.querySelectorAll('.deleteButton');
-	// Iterate all nodes
-	deletes.forEach(btn => {
-		// Attach event listener. Note to preserve the button this-reference
-		// by using the non-shorthand function
-		btn.addEventListener('click', function() {
-			var li = this.parentNode
-			li.remove();
-			EmptyList();
-			
-		})
-	})
-	
+// New function. Common for 2 use cases:
+// - for existing delete buttons, on page load
+// - for newly added delete buttons, on user action
+function activateDeleteAction(btn) {
+    btn.addEventListener('click', function() {
+        var li = this.parentNode;
+        li.remove();
+        emptyList();
+    })
+};
+
+// Renamed this function:
+function activateAllDeleteActions(){
+    var deletes = document.querySelectorAll('.deleteButton');
+    // Use other function to attach handler
+    deletes.forEach(activateDeleteAction);
 }
+
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
-		createListElement();
+		createListElement();	
 	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
-	}
+	} 
 }
 
 
 function crossOutListWhenClicked(){
 	var li = document.querySelectorAll("li");
-	// for (let index = 0; index < li.length; index++) {
-	// 	li[index].addEventListener("click", function(){
 	li.forEach(x =>
 		x.addEventListener("click", function(){
 			x.classList.toggle('done');
 		}))
 	}
 
-function EmptyList(){
+
+
+function emptyList(){	
 	var h2 = document.createElement("h2");
 	if (li.length == 0){
 		secondSection.appendChild(h2);
 		h2.append(document.createTextNode("Empty List"));
-		console.log(h2);
 	}
 }
 
-deleteListElement();
+emptyList();
+activateAllDeleteActions();
 crossOutListWhenClicked();
 addButton.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
